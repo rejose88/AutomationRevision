f57a5f2d35fa5627910a546e0f66779d89ac77d1
REENA <REENA@LAPTOP-VP7C7DUP>
2026-05-26 12:00:52 +0530

    Committing changes on 26-05-2026

---
+++ AutomationRevision/src/test/java/automationcore/Base.java
@@ -0,0 +1,25 @@
+package automationcore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+	public WebDriver driver;  
+	
+	@BeforeMethod
+		public void initialiseDriver()
+		{
+			driver= new ChromeDriver(); 
+			driver.get("https://www.saucedemo.com/");
+			driver.manage().window().maximize();
+		}
+		
+
+	/*@AfterMethod
+		public void browserCloseQuit()
+		{
+			driver.quit();
+		}   */
+
+}
